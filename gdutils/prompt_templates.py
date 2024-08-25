32285e1cd51e488572fce3276796df19a64dcf69
Greg Detre <greg@gregdetre.co.uk>
2024-08-25 20:33:15 +0100

    Tweaking LLM prompts

--- gdutils/prompt_templates.py
+++ gdutils/prompt_templates.py
@@ -1,7 +1,7 @@
 # each of these is a Jinja template. see gdutils.strings.jinja_render()
 
 summarise_text = """
-Summarise the following. Be concise, concrete, and easy to understand. Provide only the summary itself, without any superfluous conversation, commentary etc. {{ granularity }}
+Summarise the following. Be as concise, concrete, and easy to understand as you can. Provide only the summary itself, without any superfluous conversation, commentary, markup, etc. {{ granularity }}
 
 ----
 {{ txt }}
@@ -10,7 +10,7 @@ Summarise the following. Be concise, concrete, and easy to understand. Provide o
 
 # UNTESTED
 summarise_list_of_texts_as_one = """
-Summarise the whole of the following list. Be concise, concrete, and easy to understand. Provide only the summary itself, without any superfluous conversation or commentary etc. {{ granularity }}
+Summarise the whole of the following list. Be as concise, concrete, and easy to understand as you can. Provide only the summary itself, without any superfluous conversation or commentary etc. {{ granularity }}
 
 ----
 {% for txt in txts %}
